3aed9c9ac3d695cb13447521eaf15da83de3fe62
BordasDaniel <bordas.daniel0124@gmail.com>
2023-08-29 17:50:16 +0200

    commit

--- random_character.py
+++ random_character.py
@@ -3,49 +3,79 @@ import random
 import time
 
 
-class Choice:
-    def __init__(self, data):
+class Suffer:
+    def __init__(self):
         self.rerolls = 3
-        self.data = data
+        self.data = importing()
         self.current_character = None
-        self.roll()
+        self.character_roll()
+        self.current_stage = None
+        self.stage_roll()
 
-    def roll(self):
+    def startup(self):
+        while not (user := input(
+                'Are you ready to pull? (Y/N)'
+                '\nYou can also quit from the program by typing "quit"'
+                '\nYour answer: ')) == 'Y' and not user == 'y':
+            print('Sure, I can wait')
+            time.sleep(60)
+        else:
+            self.calling()
+
+    def character_roll(self):
         random_character_index = random.randint(0, len(self.data["all characters"]) - 1)
-        self.current_character = data["all characters"][random_character_index]
+        self.current_character = self.data["all characters"][random_character_index]
 
-    def writeout(self):
-        print(self.linebreaker())
-        self.roll()
-        print(f'Your current character: {self.current_character}')
-        print(self.linebreaker())
+    def stage_roll(self):
+        random_stage_index = random.randint(0, len(self.data["all stages"]) - 1)
+        self.current_stage = self.data["all stages"][random_stage_index]
+
+    def calling(self):
+        self.linebreaker()
+        self.character_roll()
+        self.stage_roll()
+        self.datas_writeout()
+        self.linebreaker()
         self.check_rerolls()
 
+    def datas_writeout(self):
+        print(
+              f"The character's name: {self.current_character['Name']}"
+              f"\nThe character's placement is in the {self.current_character['Row']}th row"
+              f"\nThe character's placement is in the {self.current_character['Column']}th column"
+              f"\nThe current stage: {self.current_stage['Stage']}"
+              )
+
     def check_rerolls(self):
-        if self.rerolls != 0:
+        if self.rerolls > 0:
             while not (user := input(
-                    f'Are you satisfied with your pull? (Y/N)\nRemember you have {self.rerolls} rerolls left.\nYour answer: ')) == 'Y' and not user == 'y' and not user == 'N' and not user == 'n':
+                    f'Are you satisfied with your pull? (Y/N)'
+                    f'\nRemember you have {self.rerolls} rerolls left.'
+                    f'\nYour answer: ')) == 'Y' and not user == 'y' and not user == 'N' and not user == 'n':
                 print('Value error\nPlease answer with (Y/N)')
             if user == 'Y' or user == 'y':
                 self.rerolls -= 1
-                self.writeout()
+                self.calling()
             else:
-                print(self.linebreaker())
-                print(f'You have selected {self.current_character["Name"]}')
-                print(self.linebreaker())
+                self.linebreaker()
+                print(f'Your final choice:')
+                self.datas_writeout()
                 self.anticheat()
         else:
             print(f"Sorry but you're out of rerolls.\nSo you have to take that.")
-            print(self.linebreaker())
+            self.linebreaker()
             self.anticheat()
 
     @staticmethod
     def welcome():
-        print("Welcome to my program!\nThis program was written in Holocure v0.6.1693021031\nIt may need some debugging if you're using the newer of the game.\nAnyways have fun and good luck!")
+        print(
+            "Welcome to my program!\nThis program was written in Holocure v0.6.1693021031"
+            "\nIt may need some debugging if you're using the newer of the game."
+            "\nAnyways have fun and good luck!")
 
     @staticmethod
     def linebreaker():
-        return f'--------------------------'
+        print(f'-------------------------------------')
 
     @staticmethod
     def anticheat():
@@ -55,23 +85,15 @@ class Choice:
             time.sleep(60)
             minutes_left -= 1
         else:
-            waiting()
+            Suffer().startup()
 
 
 def importing():
-    with open("characters.json", "r") as f:
+    with open("datas.json", "r") as f:
         return json.load(f)
 
 
-def waiting():
-    while not (user := input('Are you ready to pull? (Y/N) ')) == 'Y':
-        continue
-    else:
-        Choice(data).writeout()
-
-
 if __name__ == '__main__':
-    Choice.welcome()
-    print(Choice.linebreaker())
-    data = importing()
-    waiting()
+    Suffer.welcome()
+    Suffer.linebreaker()
+    Suffer().startup()
